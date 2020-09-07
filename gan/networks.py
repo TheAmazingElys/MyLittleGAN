3a7a3e623d2ed8bdb15276f09f62b2ed35f2a5a8
Robin ALLESIARDO <28858792+rallesiardo@users.noreply.github.com>
2020-09-07 13:27:26 +0200

    Black formattig

--- gan/networks.py
+++ gan/networks.py
@@ -16,9 +16,9 @@ def weights_init(m):
     Initialize the weight of the different layers
     """
     classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    if classname.find("Conv") != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
+    elif classname.find("BatchNorm") != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
--- tests/test_networks.py
+++ tests/test_networks.py
@@ -11,17 +11,14 @@ def test_generator_output_shape():
         latent_dim=16, img_channels=1, img_res=32, feature_map_size=32
     )
     assert (gen(gen.get_noise("cpu")).shape == np.array([1, 1, 32, 32])).all()
-    
-    
-    
+
+
 def test_discriminator_shape():
-    
+
     gen = networks.Generator(
         latent_dim=16, img_channels=1, img_res=32, feature_map_size=32
     )
-    
-    disc = networks.Discriminator(
-        img_channels=1, img_res=32, feature_map_size=16
-    )
-        
+
+    disc = networks.Discriminator(img_channels=1, img_res=32, feature_map_size=16)
+
     assert disc(gen(gen.get_noise("cpu", 5))).cpu().data.numpy().size == 5
